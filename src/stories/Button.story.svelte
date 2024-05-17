3c8b034827a92310d3076694f0e97e829d20f97b
Philippe Martin <phmartin@redhat.com>
2024-05-17 11:05:46 +0200

    set mode as control

--- src/stories/Button.story.svelte
+++ src/stories/Button.story.svelte
@@ -5,26 +5,17 @@
   import '../light.css';
   import { Button } from '@podman-desktop/ui-svelte';
 
+  let mode: 'dark-scheme' | 'light-scheme' = 'dark-scheme';
   let type: 'primary' | 'secondary' | 'danger' | 'link' | 'tab' = 'primary';
 </script>
 
 <Hst.Story>
-  <Hst.Variant title="Dark mode">
-    <div class="dark-scheme p-4 bg-[var(--pd-content-bg)]">
-      <Button {type}>content</Button>
-    </div>
-
-
-  </Hst.Variant>
-
-  <Hst.Variant title="Light mode">
-    <div class="light-scheme p-4 bg-[var(--pd-content-bg)]">
-      <Button {type}>content</Button>
-    </div>
-  </Hst.Variant>
+  <div class="{mode} p-4 bg-[var(--pd-content-bg)]">
+    <Button {type}>content</Button>
+  </div>
 
   <svelte:fragment slot="controls">
+    <Hst.Radio bind:value={mode} options="{[ { label: 'dark', value: 'dark-scheme' }, { label: 'light', value: 'light-scheme' } ]}" title="Mode" />
     <Hst.Radio bind:value={type} options="{[ { label: 'primary', value: 'primary' }, { label: 'secondary', value: 'secondary' }, { label: 'danger', value: 'danger' }, { label: 'link', value: 'link' }, { label: 'tab', value: 'tab' } ]}" title="Type" />
-</svelte:fragment>
+  </svelte:fragment>
 </Hst.Story>
-  
--- src/stories/Input.story.svelte
+++ src/stories/Input.story.svelte
@@ -5,28 +5,19 @@
   import '../dark.css';
   import '../light.css';
 
+  let mode: 'dark-scheme' | 'light-scheme' = 'dark-scheme';
   let placeholder: string = 'default placeholder';
   let disabled: boolean = false;
 </script>
 
 <Hst.Story>
-  <Hst.Variant title="Dark mode">
-    <div class="dark-scheme p-4 bg-[var(--pd-content-bg)]">
-      <Input {placeholder} {disabled}></Input>
-    </div>
-
-
-  </Hst.Variant>
-
-  <Hst.Variant title="Light mode">
-    <div class="light-scheme p-4 bg-[var(--pd-content-bg)]">
-      <Input {placeholder} {disabled}></Input>
-    </div>
-  </Hst.Variant>
+  <div class="{mode} p-4 bg-[var(--pd-content-bg)]">
+    <Input {placeholder} {disabled}></Input>
+  </div>
 
   <svelte:fragment slot="controls">
+    <Hst.Radio bind:value={mode} options="{[ { label: 'dark', value: 'dark-scheme' }, { label: 'light', value: 'light-scheme' } ]}" title="Mode" />
     <Hst.Text bind:value={placeholder} title="Placeholder" />
     <Hst.Checkbox bind:value={disabled} title="Disabled" />
-</svelte:fragment>
+  </svelte:fragment>
 </Hst.Story>
-  
